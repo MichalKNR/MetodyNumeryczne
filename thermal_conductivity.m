681c1042b848bd64dff6855ba66a9a4b6920c9c1
Michal Stasiuk <michal.stasiuk44@gmail.com>
2019-11-04 23:09:25 +0100

    task2/ created file

--- thermal_conductivity.m
+++ thermal_conductivity.m
@@ -1,7 +1,4 @@
-function [outputArg1,outputArg2] = thermal_conductivity(inputArg1,inputArg2)
-%THERMAL_CONDUCTIVITY Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function h = thermal_conductivity(T1,T2)
+
 end
 
