4e17334d0bc99d5bf84120441f0c8a6d0f81937b
Michal Stasiuk <michal.stasiuk44@gmail.com>
2019-11-04 22:23:55 +0100

    task1/working mathematical model

--- main.m
+++ main.m
@@ -7,8 +7,8 @@ mb_rod_mass = 0.2;               %parameter 3
 mw_oil_mass = 2.5;               %parameter 4
 cb_oil_heat_capacity = 3.85;     %parameter 5
 cw_oil_heat_capacity = 4.1813;   %parameter 6
-T_rod_start = 1200;              %parameter 7
-T_oil_start = 25;                %parameter 8
+T_rod_start = 1100;              %parameter 7
+T_oil_start = 70;                %parameter 8
 TIME_MAX = 3;
 time_step = 0.001;
 time = 0:time_step:TIME_MAX;
@@ -16,8 +16,8 @@ time = 0:time_step:TIME_MAX;
 %gathering all parameters as array to pass in functions
 args = [h_thermal_conductivity, A_cross_section, mb_rod_mass, mw_oil_mass, cb_oil_heat_capacity, cw_oil_heat_capacity];
 
-temperature = [1200
-                25];
+temperature = [T_rod_start:T_rod_start+3000
+                T_oil_start:T_oil_start+3000];
 temperature(:,2) = temperature(:,1) + time_step*oil_temp_transfer(temperature(:,1), temperature(:,2), args);            
 for i = 1 : length(time)-1
     time(i+1) = time(i) + time_step;
---
+++ thermal_conductivity.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = thermal_conductivity(inputArg1,inputArg2)
+%THERMAL_CONDUCTIVITY Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
