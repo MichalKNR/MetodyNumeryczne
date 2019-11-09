f6372052fb5fb42c4d67edecb2d09f0aaed197bd
Michal Stasiuk <michal.stasiuk44@gmail.com>
2019-11-09 22:52:56 +0100

    major refactor

--- main.m
+++ main.m
@@ -1,31 +1,50 @@
 clc;
 clear;
 
+do_task_3 = 0;
+
+A_cross_section =  0.0109;
+mb_rod_mass = 0.2; %task 1 version         
+mw_oil_mass = 2.5;  
+cb_rod_heat_capacity = 3.85;                      
+cw_oil_heat_capacity = 4.1813; 
+h_thermal_conductivity = 160; %task 1 version
+arguments = [A_cross_section, mb_rod_mass, mw_oil_mass, cb_rod_heat_capacity, cw_oil_heat_capacity, h_thermal_conductivity];
+
 T_rod_start = 1200;       
 T_oil_start = 25;  
 
-TIME_MAX = 2;
-time_step = 0.2;
+TIME_MAX = 3;
+time_step = 0.0001;
 time = 0:time_step:TIME_MAX;
 n = TIME_MAX/time_step;
-mw_oil_mass = 1;
+
+if do_task_3 == 1
+    calculate_oil_mass(T_oil_start, T_rod_start, arguments, time, n, time_step);
+end
 
 temperature = [T_rod_start:T_rod_start+n
                 T_oil_start:T_oil_start+n];
-while temperature(1,n) > 100
-temperature = [T_rod_start:T_rod_start+n
-                T_oil_start:T_oil_start+n];
-                       
+
 for i = 1 : length(time)-1
     time(i+1) = time_step*i;
-    temperature(:,i+1) = temperature(:,i) + time_step*oil_temp_transfer(temperature(:,i), mw_oil_mass); 
-end
-mw_oil_mass = mw_oil_mass + 1;
-temperature(:,n) %end temperature in simulation
+    temperature(:,i+1) = temperature(:,i) + time_step*oil_temp_transfer(temperature(:,i), arguments); 
 end
 
 plot_task1(time, temperature, TIME_MAX, time_step)
-mw_oil_mass
+
+final_temperature_normal = temperature(:, n);
+
+change_vulnerability = 1:length(arguments);
+
+for j = 1 : length(arguments)
+    arguments(j) = arguments(j) + arguments(j)*0.1;
+    for i = 1 : length(time)-1
+        time(i+1) = time_step*i;
+        temperature(:,i+1) = temperature(:,i) + time_step*oil_temp_transfer(temperature(:,i), arguments); 
+    end
+    %change_vulnerability(j) = (temperature(:,n) - final_temperature_normal)/arguments(j)*0.1;
+end
 
 function plot_task1(t, y, TIME_MAX, time_step)
 figure 
@@ -33,4 +52,21 @@ plot(t, y(1,:), t, y(2,:))
 title('symulator, zadanie 1')
 xlabel('czas [s]')
 ylabel('temperatura [C]')
+end
+
+function mass = calculate_oil_mass(Tw, Tb, arguments, time, n, h)
+temperature = [Tb:Tb+n
+                Tw:Tw+n];
+while temperature(1,n) > 100
+temperature = [Tb:Tb+n
+                Tw:Tw+n];
+                       
+for i = 1 : length(time)-1
+    time(i+1) = h*i;
+    temperature(:,i+1) = temperature(:,i) + h*oil_temp_transfer(temperature(:,i), arguments); 
+end
+arguments(3) = arguments(3) + 1;
+temperature(:,n) %end temperature in simulation
+end
+arguments(3)
 end
--- oil_temp_transfer.m
+++ oil_temp_transfer.m
@@ -1,18 +1,8 @@
-function dy = oil_temp_transfer(temperature_vector_prev, mass)
-A_cross_section =  0.0109;
-%mb_rod_mass = 0.2; %task 1 version
-mb_rod_mass = 0.4; %task 3 version          
-%mw_oil_mass = 2.5;  
-mw_oil_mass = mass; 
-cb_rod_heat_capacity = 3.85;                      
-cw_oil_heat_capacity = 4.1813; 
+function dy = oil_temp_transfer(temperature_vector_prev, x)
 Tb = temperature_vector_prev(1); %oil
 Tw = temperature_vector_prev(2); %rod
-%h_thermal_conductivity = thermal_conductivity(Tb,Tw); %task 2 version
-h_thermal_conductivity = 190; %task 3 version
-%h_thermal_conductivity = 160; %task 1 version
 
-dy =  [(Tw-Tb)*( h_thermal_conductivity*A_cross_section)/(mb_rod_mass*cb_rod_heat_capacity)
-       (Tb-Tw)*( h_thermal_conductivity*A_cross_section)/(mw_oil_mass*cw_oil_heat_capacity)
+dy =  [(Tw-Tb)*( x(6)*x(1))/(x(2)*x(4))
+       (Tb-Tw)*( x(6)*x(1))/(x(3)*x(5))
     ];
 end 
