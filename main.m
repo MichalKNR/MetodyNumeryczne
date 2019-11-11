686352d809afc6c3d4cc4545ea55850f79631bfe
Michal Stasiuk <michal.stasiuk44@gmail.com>
2019-11-11 23:27:21 +0100

    prepare for release 0.1

--- main.m
+++ main.m
@@ -1,45 +1,57 @@
 clc;
 clear;
 
+% decydowanie które zadanie powinno og³aszaæ swoje wyniki
 do_task_3 = 0;
-do_task_4 = 1;
+do_task_4 = 0;
 
+%podstawowe wartoœci wspó³czynników
 A_cross_section =  0.0109;
-mb_rod_mass = 0.2; %task 1 version         
+mb_rod_mass = 0.2;        
 mw_oil_mass = 2.5;  
 cb_rod_heat_capacity = 3.85;                      
 cw_oil_heat_capacity = 4.1813; 
-h_thermal_conductivity = 160; %task 1 version
+h_thermal_conductivity = 160; 
 arguments = [A_cross_section, mb_rod_mass, mw_oil_mass, cb_rod_heat_capacity, cw_oil_heat_capacity, h_thermal_conductivity];
-
 T_rod_start = 1200;       
 T_oil_start = 25;  
 
+%zmienne opisuj¹ce czas i krok symulacji
 TIME_MAX = 3;
-time_step = 0.0001;
+time_step = 0.1;
 time = 0:time_step:TIME_MAX;
 n = TIME_MAX/time_step;
 
 if do_task_3 == 1
     calculate_oil_mass(T_oil_start, T_rod_start, arguments, time, n, time_step);
-    arguments(3) = mw_oil_mass;
 end
 
+%przygotowanie wektora wynikowego
 temperature = [T_rod_start:T_rod_start+n
                 T_oil_start:T_oil_start+n];
 
+%ca³kowanie metod¹ ulepszona Eulera            
 for i = 1 : length(time)-1
     time(i+1) = time_step*i;
-    temperature(:,i+1) = temperature(:,i) + time_step*oil_temp_transfer(temperature(:,i), arguments); 
+    y_1_2 = temperature(:,i) + time_step/2*oil_temp_transfer(temperature(:,i), arguments);
+    temperature(:,i+1) = temperature(:,i) + time_step*oil_temp_transfer(y_1_2, arguments); 
+    
+    %temperature(:,i+1) = temperature(:,i) + time_step*oil_temp_transfer(temperature(:,i), arguments); 
+    %temperature(:,i+1) = temperature(:,i) + time_step*oil_temp_transfer(temperature(:,i+1), arguments);
 end
-
+% rysowanie wykresu
 final_temperature_normal = temperature(:, n);
 plot_task1(time, temperature, T_oil_start, T_rod_start, TIME_MAX,time_step, final_temperature_normal(2), final_temperature_normal(1))
 
+
+
+%wykonywanie symulacji zadania 4 dla ró¿nych wartoœci pocz¹tkowych
 if do_task_4 == 1
     change_vulnerability(final_temperature_normal, time, time_step, TIME_MAX, arguments, 1200, 25)
     change_vulnerability(final_temperature_normal, time, time_step, TIME_MAX, arguments, 700, 50)
 end
+
+
 
 function plot_task1(t, y, Tw, Tb, t_max, t_step, Tw_final, Tb_final)
 figure 
@@ -49,31 +61,35 @@ text(1.5, 500, txt,'FontSize',14)
 title('Symulator, zadanie 1')
 xlabel('czas [s]')
 ylabel('temperatura [C]')
+legend('h(delta T)')
 end
 
 function mass = calculate_oil_mass(Tw, Tb, arguments, time, n, h)
 temperature = [Tb:Tb+n
                 Tw:Tw+n];
-while temperature(1,n) > 100
-temperature = [Tb:Tb+n
+while temperature(1,n) > 100 %a¿ temperatura na koniec symulacji bêdzie mniejsza równa 100
+    %zerowanie wyników
+    temperature = [Tb:Tb+n
                 Tw:Tw+n];
-                       
-for i = 1 : length(time)-1
-    time(i+1) = h*i;
-    temperature(:,i+1) = temperature(:,i) + h*oil_temp_transfer(temperature(:,i), arguments); 
-end
-arguments(3) = arguments(3) + 0.1;
-temperature(:,n) %end temperature in simulation
-arguments(3)
+    %metoda jawna Eulera                  
+    for i = 1 : length(time)-1
+         time(i+1) = h*i;
+         temperature(:,i+1) = temperature(:,i) + h*oil_temp_transfer(temperature(:,i), arguments); 
+    end
+    %zwiêkszenie masy oleju o 0.1 kg
+    arguments(3) = arguments(3) + 0.1;
+    temperature(:,n) %dotychczas osi¹gniêta temperatura
+    arguments(3) %dotychczas osi¹gniêta masa
 end
 mass = arguments(3);
 end
 
 
 function y = change_vulnerability(final_temp, time, time_step, TIME_MAX, arguments, Tb, Tw)
+%przygotowanie wektora
 temperature = [Tb:Tb+length(time)-1
                 Tw:Tw+length(time)-1];
-            
+%    przygotowanie tablic wra¿liwoœci na zmiany        
 args_vulnerability = [1:length(arguments)
                         1:length(arguments)];
 time_vulnerability = [1:length(arguments)
@@ -85,22 +101,26 @@ for j = 1 : length(arguments)
     delta_arg = 0.1;
     args = arguments;
     args(j) = args(j) + args(j)*delta_arg;
+    %metoda jawna Eulera
     for i = 1 : length(time)-1
         time(i+1) = time_step*i;
         temperature(:,i+1) = temperature(:,i) + time_step*oil_temp_transfer(temperature(:,i), args); 
     end 
+    %obliczenie pochodnej cz¹stkowej
     args_vulnerability(:,j) = (temperature(:,end) - final_temp)/(args(j)*delta_arg);
     
     n = TIME_MAX/time_step_table(j);
     temperature = [Tb:Tb+n-1
                 Tw:Tw+n-1];
+    %metoda jawna Eulera
     for i = 1 : n-1
         time(i+1) = time_step_table(j)*i;
         temperature(:,i+1) = temperature(:,i) + time_step_table(j)*oil_temp_transfer(temperature(:,i), args); 
     end 
-    time_vulnerability(:,j) = (temperature(:,end) - final_temp)/10;
+    %obliczenie pochodnej cz¹stkowej
+    time_vulnerability(:,j) = (temperature(:,end) - final_temp)/(time_step/time_step_table(j));
 end
-
+%wypisanie tabel
 args_vulnerability
 time_vulnerability
 end
--- oil_temp_transfer.m
+++ oil_temp_transfer.m
@@ -2,6 +2,7 @@ function dy = oil_temp_transfer(temperature_vector_prev, x)
 Tb = temperature_vector_prev(1); %oil
 Tw = temperature_vector_prev(2); %rod
 
+%równania stanu
 dy =  [(Tw-Tb)*( x(6)*x(1))/(x(2)*x(4))
        (Tb-Tw)*( x(6)*x(1))/(x(3)*x(5))
     ];
